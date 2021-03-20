8c3878261c157f9cd2887519f2688415e9a7510c
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-03-20 12:08:29 -0400

    add new player images

--- src/data/Player.ts
+++ src/data/Player.ts
@@ -2,7 +2,7 @@ export const maxPlayers = 4;
 
 export const minPlayers = 2;
 
-export const playerOptions = [ 'red', 'blue', 'green', 'yellow' ].map(color => ({
+export const playerOptions = [ 'red', 'blue', 'brown', 'purple' ].map(color => ({
 	name: color,
 	value: `Player-${color}`
 }));
