f9a8790a303416343a44e36ef964c04544f1e688
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-03-20 11:44:29 -0400

    add the newly designed board

--- src/data/Bridges.ts
+++ src/data/Bridges.ts
@@ -2,6 +2,6 @@ import { Tile } from '../model';
 import { tiles } from './Tiles';
 
 export const bridges: Map<Tile, Tile> = new Map([
-	[ tiles[4], tiles[58] ],
-	[ tiles[33], tiles[46] ]
+	[ tiles[4], tiles[62] ],
+	[ tiles[38], tiles[49] ]
 ]);
--- src/data/DotTiles.ts
+++ src/data/DotTiles.ts
@@ -2,7 +2,7 @@ import { Tile } from '../model';
 import { tiles } from './Tiles';
 
 export const dotTiles: Set<Tile> = new Set([
-	tiles[47],
+	tiles[53],
 	tiles[85],
 	tiles[120]
 ]);
