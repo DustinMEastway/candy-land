b0c9ed6ed3443eb3e2e9f15797519654cef833b3
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-04-03 14:07:06 -0400

    refactor game service to handle player turns

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -5,60 +5,12 @@ import { AppContext } from './AppContext';
 import { CardType, Tile, TileType } from './model';
 import './Deck.scss';
 
-const getNextTile = (tiles: Tile[], type: TileType) => tiles.find(tile => tile.type === type);
-
 export const Deck: React.FC = () => {
 	const { gameService } = useContext(AppContext);
-	const { save } = gameService;
-	const [ lastCard, setLastCard ] = useSubject(save, s => s.lastCard);
-	const [ players, setPlayers ] = useSubject(save, s => s.players);
-	const [ , setState ] = useSubject(save, s => s.state);
-	const [ turn, setTurn ] = useSubject(save, s => s.turn);
+	const [ lastCard ] = useSubject(gameService.save, s => s.lastCard);
 
 	return <div className="Deck">
-		<button className="Deck-pile Deck-draw-pile" onClick={() => {
-			const player = players[turn];
-			const card = gameService.drawCard();
-			const isDouble = card.startsWith('2_');
-			const color = card.replace(/^2_/, '') as TileType;
-
-			const currentPlayerTile = tiles[player.position];
-			let tilesToSearch: Tile[];
-			if ((characterCards as CardType[]).includes(card)) {
-				tilesToSearch = tiles;
-			} else {
-				tilesToSearch = tiles.slice(tiles.indexOf(currentPlayerTile) + 1);
-			}
-
-			let tile: Tile | undefined;
-			if (dotTiles.has(currentPlayerTile) && currentPlayerTile.type !== color) {
-				// keep player where they are if they do not draw the correct color for a dot tile
-				tile = currentPlayerTile;
-			} else {
-				tile = getNextTile(tilesToSearch, color);
-				if (tile && isDouble) {
-					tile = getNextTile(tiles.slice(tiles.indexOf(tile) + 1), color);
-				}
-			}
-
-			if (tile && bridges.has(tile)) {
-				tile = bridges.get(tile);
-			} else if (!tile) {
-				// if no tile is found, then the player is at the end which is a rainbow tile
-				tile = tiles[tiles.length - 1];
-			}
-
-			setLastCard(card);
-			setPlayers(players.map((p, i) =>
-				(i === turn && tile) ? { ...p, position: tiles.indexOf(tile) } : p
-			));
-			if (tile === tiles[tiles.length - 1]) {
-				setState('complete');
-			} else {
-				setTurn((turn + 1) % players.length);
-			}
-		}}>
-		</button>
+		<button className="Deck-pile Deck-draw-pile" onClick={() => gameService.takeTurn()}></button>
 		<div className={'Deck-pile Deck-discard-pile' + (lastCard ? ` Deck-discard-pile--${lastCard}` : '')}></div>
 	</div>;
 };
--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,50 +1,77 @@
 import {
+	bridges,
 	characterCards,
 	colorCards,
 	colorCardsCount,
+	dotTiles,
 	doubleColorCardsCount,
 	maxPlayers,
-	playerOptions
+	playerOptions,
+	tiles
 } from '../data';
-import { CardType, GameSave } from '../model';
+import { CardType, GameSave, Tile, TileType } from '../model';
 import { range, shuffle, Subject } from '../lib';
 
 export class GameService {
 	/** Save containing the current game state. */
-	readonly save = new Subject<GameSave>(this.createNewSave());
-
-	/** Draw the card at the top of the deck. */
-	drawCard(): CardType {
-		const save = { ...this.save.value };
-		const deck = (save.deck.length) ? save.deck : this.createDeck();
-		save.lastCard = deck[0];
-		save.deck = deck.slice(1);
-		this.save.next(save);
-
-		return save.lastCard;
-	}
+	readonly save = new Subject<GameSave>(GameService.createNewSave());
 
 	/** Reset @see save to a new game. */
 	startNewGame(): void {
-		this.save.next(this.createNewSave());
+		this.save.next(GameService.createNewSave());
 	}
 
-	protected createNewSave(): GameSave {
-		return ({
-			deck: this.createDeck(),
-			lastCard: null,
-			players: playerOptions.slice(0, maxPlayers).map(({ name, value }) => ({
-				className: value,
-				id: value,
-				name: name.substring(0, 1).toUpperCase() + name.substring(1),
-				position: 0
-			})),
-			state: 'new',
-			turn: 0
-		});
+	/** Have the current player take a turn. */
+	takeTurn(): void {
+		let currentSave = { ...this.save.value };
+		const { players, turn } = currentSave;
+		const player = players[turn];
+		currentSave = GameService.drawCard(currentSave);
+		const card = currentSave.lastCard!;
+		const isDouble = card.startsWith('2_');
+		const color = card.replace(/^2_/, '') as TileType;
+
+		const currentPlayerTile = tiles[player.position];
+		let tilesToSearch: Tile[];
+		if ((characterCards as CardType[]).includes(card)) {
+			tilesToSearch = tiles;
+		} else {
+			tilesToSearch = tiles.slice(tiles.indexOf(currentPlayerTile) + 1);
+		}
+
+		let tile: Tile | undefined;
+		if (dotTiles.has(currentPlayerTile) && currentPlayerTile.type !== color) {
+			// keep player where they are if they do not draw the correct color for a dot tile
+			tile = currentPlayerTile;
+		} else {
+			tile = GameService.getNextTile(tilesToSearch, color);
+			if (tile && isDouble) {
+				tile = GameService.getNextTile(tiles.slice(tiles.indexOf(tile) + 1), color);
+			}
+		}
+
+		if (tile && bridges.has(tile)) {
+			tile = bridges.get(tile);
+		} else if (!tile) {
+			// if no tile is found, then the player is at the end which is a rainbow tile
+			tile = tiles[tiles.length - 1];
+		}
+
+		currentSave.lastCard = card;
+		currentSave.players = players.map((p, i) =>
+			(i === turn && tile) ? { ...p, position: tiles.indexOf(tile) } : p
+		);
+		if (tile === tiles[tiles.length - 1]) {
+			currentSave.state = 'complete';
+		} else {
+			currentSave.turn = (turn + 1) % players.length;
+		}
+
+		this.save.next(currentSave);
 	}
 
-	protected createDeck(): CardType[] {
+	/** Create a new shuffled deck. */
+	protected static createDeck(): CardType[] {
 		const deck = ([] as CardType[]).concat(
 			// color cards
 			range(colorCardsCount).map(i => colorCards[i % colorCards.length]),
@@ -54,5 +81,36 @@ export class GameService {
 		);
 
 		return shuffle(deck);
+	}
+
+	/** Create a new save. */
+	protected static createNewSave(): GameSave {
+		return ({
+			deck: this.createDeck(),
+			lastCard: null,
+			players: playerOptions.slice(0, maxPlayers).map(({ name, value }) => ({
+				className: value,
+				id: value,
+				name: name.substring(0, 1).toUpperCase() + name.substring(1),
+				position: 0
+			})),
+			state: 'in-progress',
+			turn: 0
+		});
+	}
+
+	/** Draw the card at the top of the deck. */
+	protected static drawCard(currentSave: GameSave): GameSave {
+		const save = { ...currentSave };
+		const deck = (save.deck.length) ? save.deck : this.createDeck();
+		save.lastCard = deck[0];
+		save.deck = deck.slice(1);
+
+		return save;
+	}
+
+	/** Gets the first tile from @see tiles of the requested @type. */
+	protected static getNextTile(tiles: Tile[], type: TileType) {
+		return tiles.find(tile => tile.type === type);
 	}
 }
