81cb1622c971ba01abedbabdc29c531b11733dd2
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-04-04 10:08:56 -0400

    rename players brown => orange, red => pink

--- src/data/Player.ts
+++ src/data/Player.ts
@@ -4,7 +4,7 @@ export const maxPlayers = 4;
 
 export const minPlayers = 2;
 
-export const playerOptions: PlayerOption[] = [ 'Red', 'Blue', 'Brown', 'Purple' ].map(color => ({
+export const playerOptions: PlayerOption[] = [ 'Pink', 'Blue', 'Orange', 'Purple' ].map(color => ({
 	name: color,
 	value: `Player-${color.toLowerCase()}`
 }));
